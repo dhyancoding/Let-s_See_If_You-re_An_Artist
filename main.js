6bfae271dc3dac86a69c470e03e61de9c599f5ff
dhyancoding <142217143+dhyancoding@users.noreply.github.com>
2024-02-16 18:35:09 +1030

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+function preload(){
+    classifier = ml5.imageClassifier("DoodleNet")
+}
+
+function setup(){
+    canvas=createCanvas(280, 280)
+    canvas.center()
+    background("white")
+    canvas.mouseReleased(ClassifyCanvas)
+    s = window.SpeechSynthesis
+
+}
+
+function draw(){
+    strokeWeight(15)
+    stroke(0)
+    if(mouseIsPressed){
+        line(pmouseX, pmouseY, mouseX, mouseY)
+    }
+}
+
+function ClassifyCanvas(){
+    classifier.classify(canvas, gotResults)
+}
+
+function gotResults(error, results){
+    if(error){
+        console.error(error)
+    }
+    else{
+        console.log(results)
+        document.getElementById("label").innerHTML = "Drawing: " + results[0].label
+        document.getElementById("confidence").innerHTML = "Drawing: " + Math.round(results[0].confidence*100) + " %"
+        u = new SpeechSynthesisUtterance(results[0].label)
+        s.speak(u)
+    }
+} 
+
+function Clear_canvas(){
+    background("white")
+}
